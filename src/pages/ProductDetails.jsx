2903f9e9148440a5bc276337ab381b5dbc7dbbe5
Alejandro Hau <ahcervantes339@gmail.com>
2025-12-13 21:31:31 -0600

    Lógica completada, siguiente paso acomodar css

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,44 @@
+import { useParams } from "react-router-dom"
+import { products } from '../data/products'
+import { useCart } from '../context/CartContext'
+
 function ProductDetails() {
+    const { id } = useParams() //id de la url
+
+    //buscar producto en la lista (convertir a numero)
+    const product = products.find(p => p.id === parseInt(id))
+
+    const { addToCart } = useCart()
+
+    if (!product) {
+        return <h2>Producto no encontrado</h2>
+    }
+
     return (
-        <div>
-            <h1>Detalles del Producto</h1>
+        <div style={{ padding: '20px', display: 'flex', gap: '30px' }}>
+            {/* izquierda: imagen */}
+            <img
+                src={product.image}
+                alt={product.title}
+                style={{ width: '300px', height: '300px', objectFit: 'contain' }}
+            />
+
+            {/* derecha: info */}
+            <div>
+                <h1>{product.title}</h1>
+                <h2 style={{ color: 'green' }}>${product.price}</h2>
+                <p>{product.description}</p>
+                <p><strong>Categoría:</strong>{product.category}</p>
+
+                <button
+                    onClick={() => addToCart(product)}
+                    style={{ background: 'black', color: 'white', padding: '10px 20px', fontSize: '16px', cursor: 'pointer', marginTop: '20px' }}
+                >Agregar al carrito
+                </button>
+
+            </div>
         </div>
     )
 }
+
 export default ProductDetails
